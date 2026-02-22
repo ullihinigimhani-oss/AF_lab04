d90963e75fcd659fba334737708464bfda307451
GIMHANI U L L <it23600270@my.sliit.lk>
2026-02-22 11:43:12 +0530

    Add Node.js module usage example

--- app.js
+++ app.js
@@ -17,19 +17,23 @@
 //   console.log('Server running at http://localhost:3000');
 // });
 
-const https = require('https');
+// const https = require('https');
 
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-  let data = '';
+// https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+//   let data = '';
 
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+//   resp.on('data', (chunk) => {
+//     data += chunk;
+//   });
 
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
-  });
+//   resp.on('end', () => {
+//     console.log(JSON.parse(data));
+//   });
 
-}).on('error', (err) => {
-  console.log("Error: " + err.message);
-});
+// }).on('error', (err) => {
+//   console.log("Error: " + err.message);
+// });
+
+const myModule = require('./my-module.js');
+
+console.log(myModule.myFunction());
