0c99f93df8a1b94c8101d529d7038d7f219d19e0
GIMHANI U L L <it23600270@my.sliit.lk>
2026-02-22 11:37:15 +0530

    Add Node.js HTTP request example

--- app.js
+++ app.js
@@ -5,14 +5,31 @@
 //   console.log('File saved!');
 // });
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('Hello World!');
-  res.end();
-});
+// const server = http.createServer(function (req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write('Hello World!');
+//   res.end();
+// });
 
-server.listen(3000, function () {
-  console.log('Server running at http://localhost:3000');
+// server.listen(3000, function () {
+//   console.log('Server running at http://localhost:3000');
+// });
+
+const https = require('https');
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
 });
