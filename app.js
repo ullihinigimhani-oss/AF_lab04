d591a1db85ba71b75a231ab0d357fa31ea7511f1
GIMHANI U L L <it23600270@my.sliit.lk>
2026-02-22 11:47:13 +0530

    Add Node.js promises example

--- app.js
+++ app.js
@@ -34,6 +34,24 @@
 //   console.log("Error: " + err.message);
 // });
 
-const myModule = require('./my-module.js');
+// const myModule = require('./my-module.js');
 
-console.log(myModule.myFunction());
+// console.log(myModule.myFunction());
+
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
